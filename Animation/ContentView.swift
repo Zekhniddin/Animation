525b12ebd722b0bc8c6d89470f6703c11bd80bae
Zekhniddin <zexjumayev@gmail.com>
2021-01-12 21:42:01 +0500

    ...

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var angleDegreeChanged = false
+    @State private var sizeChanged = false
+    @State private var opacityChanged = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image("visa_card")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .opacity(opacityChanged ? 0.4 : 1.0)
+            .scaleEffect(sizeChanged ? 0.4 : 1)
+            .rotationEffect(Angle(degrees: angleDegreeChanged ? 360 : 0))
+            .animation(.spring(response: 1.0, dampingFraction: 0.8))
+            .onTapGesture {
+                self.sizeChanged.toggle()
+                self.opacityChanged.toggle()
+                self.angleDegreeChanged.toggle()
+            }
     }
 }
 
